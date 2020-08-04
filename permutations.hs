603133cf5b9bf73e246443e15d5e3a9a63019b7d
zhuolinho <zhuolinho@hotmail.com>
2020-08-04 14:20:47 +0800

    permutations

--- permutations.hs
+++ permutations.hs
@@ -6,19 +6,28 @@
 -- likes [x, y] = x ++ " and " ++ y ++ " like this"
 -- likes [x, y, z] = x ++ ", " ++ y ++ " and " ++ z ++ " like this"
 -- likes (x : y : lst) = x ++ ", " ++ y ++ " and " ++ (show (length lst)) ++ " others like this"
+import Data.List (nub)
 
 insert :: Char -> String -> [String]
 insert a str = [(take n str) ++ (a : (drop n str)) | n <- [0 .. length str]]
 
-unique :: [String] -> [String]
-unique [] = []
-unique (x : xs) = x : filter (/= x) (unique xs)
-
 append :: [[String]] -> [String]
 append [] = []
-append (x : xs) = unique (x ++ append xs)
+append (x : xs) = x ++ append xs
 
 permutations :: String -> [String]
 permutations [] = [""]
 permutations [x] = [[x]]
-permutations (x : xs) = append [(insert x t) | t <- (permutations xs)]
+permutations (x : xs) = nub (append [(insert x t) | t <- (permutations xs)])
+
+-- data Base = A | T | G | C
+-- type DNA = [Base]
+
+-- f :: Base -> Base
+-- f A = T
+-- f T = A
+-- f G = C
+-- f C = G
+
+-- dnaStrand :: DNA -> DNA
+-- dnaStrand xs = map f xs
