e3278c76f911cce1fbfce233323dd3dc33d2bb1a
zhuolinho <zhuolinho@hotmail.com>
2020-08-12 20:02:26 +0800

    interactWith

--- InteractWith.hs
+++ InteractWith.hs
@@ -3,7 +3,7 @@ import           System.Environment (getArgs)
 
 interactWith function inputFile outputFile = do
   input <- readFile inputFile
-  writeFile outputFile (function input)
+  writeFile outputFile $ function input
 
 main = mainWith myFunction
   where
@@ -14,7 +14,7 @@ main = mainWith myFunction
         _ -> putStrLn "error: exactly two arguments needed"
 
     -- replace "id" with the name of our function below
-    myFunction x = unlines $ concat $ zipWithN (++) $ map (map (:[])) $ lines x
+    myFunction = unlines . concat . zipWithN (++) . map (map (:[])) . lines
 
     zipWithN func [] = []
     zipWithN func [x] = [x]
