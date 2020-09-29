16d38dd3ea25d862415c7411831b5a29de17b7a3
zhuolinho <zhuolinho@hotmail.com>
2020-09-29 16:19:40 +0800

    JObject & JArray

--- JSONClass.hs
+++ JSONClass.hs
@@ -1,25 +1,14 @@
 -- file: ch06/JSONClass.hs
 {-# LANGUAGE FlexibleInstances #-}
 
-import Control.Arrow (second)
+import           Control.Arrow (second)
 
-newtype JAry a = JAry
-  { fromJAry :: [a]
-  }
-  deriving (Eq, Ord, Show)
-
-newtype JObj a = JObj
-  { fromJObj :: [(String, a)]
-  }
-  deriving (Eq, Ord, Show)
-
-data JValue
-  = JString String
-  | JNumber Double
-  | JBool Bool
-  | JNull
-  | JObject (JObj JValue)
-  | JArray (JAry JValue)
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
   deriving (Eq, Ord, Show)
 
 type JSONError = String
@@ -30,15 +19,18 @@ class JSON a where
 
 instance JSON JValue where
   toJValue = id
+
   fromJValue = Right
 
 instance JSON Bool where
   toJValue = JBool
+
   fromJValue (JBool b) = Right b
   fromJValue _ = Left "not a JSON boolean"
 
 instance JSON String where
   toJValue = JString
+
   fromJValue (JString s) = Right s
   fromJValue _ = Left "not a JSON string"
 
@@ -48,65 +40,56 @@ doubleToJValue _ _ = Left "not a JSON number"
 
 instance JSON Int where
   toJValue = JNumber . realToFrac
+
   fromJValue = doubleToJValue round
 
 instance JSON Integer where
   toJValue = JNumber . realToFrac
+
   fromJValue = doubleToJValue round
 
+instance JSON Double where
+  toJValue = JNumber
+
+  fromJValue = doubleToJValue id
+
 mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
-mapEithers f (x : xs) = case mapEithers f xs of
+mapEithers f (x:xs) = case mapEithers f xs of
   Left err -> Left err
   Right ys -> case f x of
     Left err -> Left err
-    Right y -> Right (y : ys)
+    Right y  -> Right (y:ys)
 mapEithers _ _ = Right []
 
 whenRight :: (b -> c) -> Either a b -> Either a c
 whenRight _ (Left err) = Left err
 whenRight f (Right a) = Right (f a)
 
-jaryFromJValue :: (JSON a) => JValue -> Either JSONError (JAry a)
-jaryFromJValue (JArray (JAry a)) =
-  whenRight JAry (mapEithers fromJValue a)
-jaryFromJValue _ = Left "not a JSON array"
+instance {-# OVERLAPPABLE #-}(JSON a) => JSON [a] where
+  toJValue = JArray . map toJValue
 
-jaryToJValue :: (JSON a) => JAry a -> JValue
-jaryToJValue = JArray . JAry . map toJValue . fromJAry
+  fromJValue (JArray a) = whenRight id (mapEithers fromJValue a)
+  fromJValue _ = Left "not a JSON array"
 
-instance (JSON a) => JSON (JAry a) where
-  toJValue = jaryToJValue
-  fromJValue = jaryFromJValue
+instance (JSON a) => JSON [(String, a)] where
+  toJValue = JObject . map (second toJValue)
 
-instance (JSON a) => JSON (JObj a) where
-  toJValue = JObject . JObj . map (second toJValue) . fromJObj
-
-  fromJValue (JObject (JObj o)) = whenRight JObj (mapEithers unwrap o)
+  fromJValue (JObject o) = whenRight id (mapEithers unwrap o)
     where
       unwrap (k, v) = whenRight ((,) k) (fromJValue v)
   fromJValue _ = Left "not a JSON object"
 
 result :: JValue
-result =
-  JObject
-    ( JObj
-        [ ("query", JString "awkward squad haskell"),
-          ("estimatedCount", JNumber 3920),
-          ("moreResults", JBool True),
-          ( "results",
-            JArray
-              ( JAry
-                  [ JObject
-                      ( JObj
-                          [ ("title", JString "Simon Peyton Jones: papers"),
-                            ("snippet", JString "Tackling the awkward ..."),
-                            ("url", JString "http://.../marktoberdorf/")
-                          ]
-                      )
-                  ]
-              )
-          )
-        ]
-    )
+result = JObject
+  [ ("query", JString "awkward squad haskell")
+  , ("estimatedCount", JNumber 3920)
+  , ("moreResults", JBool True)
+  , ( "results"
+      , JArray
+        [ JObject
+            [ ("title", JString "Simon Peyton Jones: papers")
+            , ("snippet", JString "Tackling the awkward ...")
+            , ("url", JString "http://.../marktoberdorf/")]])]
 
-abc = (fromJValue result) :: Either JSONError (JAry JValue)
+abc = (fromJValue (toJValue [("fa", (123 :: Int)), ("de", (13 :: Int))]))
+  :: Either JSONError [(String, Integer)]
