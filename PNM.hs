fcbb3f491d1d40cc3d928f75e2c3cf6e2888c92d
zhuolinho <zhuolinho@hotmail.com>
2020-10-21 01:12:57 +0800

    parseBytes

--- PNM.hs
+++ PNM.hs
@@ -1,6 +1,6 @@
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Data.ByteString.Lazy as L
-import           Data.Char (isSpace)
+import           Data.Char (isSpace, chr, isDigit)
 import           Data.Int (Int64)
 import           Data.Word (Word8)
 
@@ -118,5 +118,71 @@ bail :: String -> Parse a
 bail err = Parse
   $ \s -> Left $ "byte offset " ++ show (offset s) ++ ": " ++ err
 
-abc = runParse (parseByte ==> \_ -> parseByte ==> \_ -> parseByte)
-  $ ParseState (L8.pack "ab") 0
+instance Functor Parse where
+  fmap f parser = parser ==> \result -> identity (f result)
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+-- import Control.Applicative
+parseChar :: Parse Char
+parseChar = w2c <$> parseByte
+
+peekByte :: Parse (Maybe Word8)
+peekByte = fmap fst <$> L.uncons <$> string <$> getState
+
+peekChar = fmap w2c <$> peekByte
+
+parseWhile :: (Word8 -> Bool) -> Parse [Word8]
+parseWhile p = (fmap p <$> peekByte)
+  ==> \mp -> if mp == Just True
+             then parseByte ==> \b -> (b:) <$> parseWhile p
+             else identity []
+
+parseRawPGM :: Parse Greymap
+parseRawPGM = parseWhileWith w2c notWhite
+  ==> \header
+  -> skipSpaces ==>& assert (header == "P5") "invalid raw header" ==>& parseNat
+  ==> \width -> skipSpaces ==>& parseNat
+  ==> \height -> skipSpaces ==>& parseNat
+  ==> \maxGrey -> parseByte ==>& parseBytes (width * height)
+  ==> \bitmap -> identity (Greymap width height maxGrey bitmap)
+  where
+    notWhite = (`notElem` " \r\n\t")
+
+parseWhileWith :: (Word8 -> a) -> (a -> Bool) -> Parse [a]
+parseWhileWith f p = fmap f <$> parseWhile (p . f)
+
+parseBytes :: Int -> Parse L.ByteString
+parseBytes p = parseCount p
+  ==> \words -> if length words < p
+                then bail "no more input"
+                else identity (L8.pack words)
+
+parseCount :: (Eq t, Num t) => t -> Parse [Char]
+parseCount 0 = identity []
+parseCount p = peekByte
+  ==> \mp -> if mp == Nothing
+             then identity []
+             else parseChar ==> \b -> (b:) <$> parseCount (p - 1)
+
+abc = parse (parseBytes 5) (L8.pack "12oo")
+
+parseNat :: Parse Int
+parseNat = parseWhileWith w2c isDigit
+  ==> \digits -> if null digits
+                 then bail "no more input"
+                 else let n = read digits
+                      in if n < 0
+                         then bail "integer overflow"
+                         else identity n
+
+(==>&) :: Parse a -> Parse b -> Parse b
+p ==>& f = p ==> \_ -> f
+
+skipSpaces :: Parse ()
+skipSpaces = parseWhileWith w2c isSpace ==>& identity ()
+
+assert :: Bool -> String -> Parse ()
+assert True _ = identity ()
+assert False err = bail err
