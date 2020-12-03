dbe9b4da9d4128cea0f319743a8e90f5019d279a
zhuolinho <zhuolinho@hotmail.com>
2020-12-03 09:36:02 +0800

    MonadSupply

--- ch15/Supply.hs
+++ ch15/Supply.hs
@@ -6,7 +6,7 @@ import           Control.Monad.State (liftM2, runState, MonadState(put, get)
                                     , State, StateT(StateT))
 
 newtype Supply s a = S (State [s] a)
-  deriving (Functor, Applicative)
+  deriving (Functor, Applicative, Monad)
 
 runSupply :: Supply s a -> [s] -> (a, [s])
 runSupply (S m) xs = runState m xs
@@ -21,12 +21,15 @@ next = S
         put xs
         return (Just x)
 
-unwrapS :: Supply s a -> State [s] a
-unwrapS (S s) = s
-
-instance Monad (Supply s) where
-  s >>= m = S (unwrapS s >>= unwrapS . m)
-
-  return = S . return
-
+-- unwrapS :: Supply s a -> State [s] a
+-- unwrapS (S s) = s
+-- instance Monad (Supply s) where
+--   s >>= m = S (unwrapS s >>= unwrapS . m)
+--   return = S . return
 abc = runSupply (liftM2 (,) next next) [1, 2, 3]
+
+showTwo :: (Show s) => Supply s String
+showTwo = do
+  a <- next
+  b <- next
+  return (show "a: " ++ show a ++ ", b: " ++ show b)
