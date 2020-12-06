a2e3cb0819b5c1db4017c0689e88a6253d931beb
zhuolinho <zhuolinho@hotmail.com>
2020-12-06 18:20:48 +0800

    MonadHandle

---
+++ ch15/MonadHandleIO.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE FunctionalDependencies #-}
+import MonadHandle
+import qualified System.IO
+
+import System.IO (IOMode(..))
+import Control.Monad.Trans (MonadIO(..), MonadTrans(..))
+import System.Directory (removeFile)
+
+import SafeHello
+
+instance MonadHandle System.IO.Handle IO where
+        openFile = System.IO.openFile
+        hPutStr = System.IO.hPutStr
+        hClose = System.IO.hClose
+        hGetContents = System.IO.hGetContents
+        hPutStrLn = System.IO.hPutStrLn
