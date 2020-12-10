61d772086fe6a25fcaae71ad4e677556d2745337
zhuolinho <zhuolinho@hotmail.com>
2020-12-10 23:58:15 +0800

    FormParse

---
+++ ch16/FormParse.hs
@@ -0,0 +1,24 @@
+import           Text.ParserCombinators.Parsec
+import           Numeric
+
+p_query :: CharParser () [(String, Maybe String)]
+p_query = p_pair `sepBy` char '&'
+
+p_pair :: CharParser () (String, Maybe String)
+p_pair = do
+  name <- many1 p_char
+  value <- optionMaybe (char '=' >> many p_char)
+  return (name, value)
+
+p_char :: CharParser () Char
+p_char = oneOf urlBaseChars <|> (char '+' >> return ' ') <|> p_hex
+
+urlBaseChars = ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9'] ++ "$-_.!*'(),"
+
+p_hex :: CharParser () Char
+p_hex = do
+  char '%'
+  a <- hexDigit
+  b <- hexDigit
+  let ((d, _):_) = readHex [a, b]
+  return . toEnum $ d
